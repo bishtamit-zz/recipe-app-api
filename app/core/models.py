69442ba56273c7a1edc0e26156ca9b2cee9dded8
amit bisht <amit.bisht@c-zentrix.com>
2021-01-28 16:50:41 +0530

    added custom user model with test

--- app/core/models.py
+++ app/core/models.py
@@ -7,8 +7,19 @@ from django.db import models
 
 class UserManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("email is necessary")
+        email = self.normalize_email(email)
         user = self.model(email=email, **extra_fields)
         user.set_password(password)
+        user.save(using=self.db)
+
+        return user
+
+    def create_superuser(self, email, password):
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
         user.save(using=self.db)
 
         return user
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -13,3 +13,23 @@ class ModelTests(TestCase):
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        email = 'amitbisht@GMAIL.com'
+        user = get_user_model().objects.create_user(
+            email,
+            'test1235'
+        )
+        self.assertEqual(user.email, email.lower())
+
+    def test_new_user_invalid_email(self):
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, 'test2234')
+
+    def test_create_new_super_user(self):
+        user = get_user_model().objects.create_superuser(
+            'amit@gmail.com',
+            'test1234'
+        )
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
