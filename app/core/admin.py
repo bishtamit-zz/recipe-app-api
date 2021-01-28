16c337e0be1796c55c760bc837e46cac0eaf1dca
amit bisht <amit.bisht@c-zentrix.com>
2021-01-28 17:44:57 +0530

    admin added for the custom user and test created

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,31 @@
-# from django.contrib import admin
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext as _
+
+from core.models import User
+
 
 # Register your models here.
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    ordering = ['id']
+    list_display = ['name', 'email']
+    fieldsets = (
+        (None, {
+            'fields': ('email', 'password')
+        }),
+        (_('Personal Info'), {
+            'fields': ('is_active', 'is_staff', 'is_superuser')
+        }),
+        (_('Important Dates'), {
+            'fields': ('last_login',)
+        })
+    )
+
+    add_fieldsets = [
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2')
+        })
+    ]
